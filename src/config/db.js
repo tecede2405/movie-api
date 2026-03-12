6ebae1c6d5b123523a5577c2f26c5a8567ee006f
thoaiphan123 <ptoan5950@gmail.com>
2026-03-12 19:13:30 +0700

    fix connect DB

--- src/config/db.js
+++ src/config/db.js
@@ -6,18 +6,25 @@ let cached = global.mongoose;
 if (!cached) cached = global.mongoose = { conn: null, promise: null };
 
 async function connectDB() {
-  if (cached.conn) return cached.conn;
+  if (cached.conn) {
+    console.log("⚡ Using cached Mongo connection");
+    return cached.conn;
+  }
 
   if (!cached.promise) {
+    console.log("⏳ Connecting to MongoDB...");
+
     cached.promise = mongoose.connect(MONGO_URI, {
       bufferCommands: false,
-      serverSelectionTimeoutMS: 5000, 
+      serverSelectionTimeoutMS: 5000,
       socketTimeoutMS: 45000,
-    }).then(m => m);
+    }).then((m) => {
+      console.log("✅ Mongo connected");
+      return m;
+    });
   }
 
   cached.conn = await cached.promise;
-  console.log("✅ Mongo connected");
   return cached.conn;
 }
 
